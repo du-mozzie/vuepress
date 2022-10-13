ee07065fab4d63df041c6b7a1e1c663f5f1c1719
Du <2548425238@qq.com>
2022-10-13 11:25:31 +0800

    修改title

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 const nav = require("./nav");
 const sidebar = require("./sidebar");
 module.exports = {
-  title: "Coder du",
+  title: "Wave",
   description: "This is my personal blog",
   port: "80",
   dest: "public",
